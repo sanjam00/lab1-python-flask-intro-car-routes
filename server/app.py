0ab70a6e747851f95ca356772e749c7323d53a18
Sanaya James <sanaya.j.james@gmail.com>
2026-03-04 19:34:44 -0700

    set up / route and auto-run

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return "<h1>Welcome to Flatiron Cars</h1>"
+
+if __name__ =='__main__':
+  app.run(port=555, debug=True)
