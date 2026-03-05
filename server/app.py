ede19b1fd4c2966eb3ad0bea65906ad52637e133
Sanaya James <sanaya.j.james@gmail.com>
2026-03-04 19:43:05 -0700

    implemented model routing based on list of existing cars

--- server/app.py
+++ server/app.py
@@ -1,12 +1,19 @@
-existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
-
 from flask import Flask
 
 app = Flask(__name__)
 
+existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
 @app.route('/')
 def index():
-  return "<h1>Welcome to Flatiron Cars</h1>"
+  return "Welcome to Flatiron Cars"
+
+@app.route('/<model>')
+def model(model):
+  if model in existing_models:
+    return f'Flatiron {model} is in our fleet!'
+  else:
+    return f'No models called {model} exists in our catalog'
 
 if __name__ =='__main__':
   app.run(port=555, debug=True)
